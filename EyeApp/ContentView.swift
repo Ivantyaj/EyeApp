d1e0f946fae21c7b035b4f1fc2a5fa5cf5840b96
Ivantyaj <39460276+Ivantyaj@users.noreply.github.com>
2020-06-25 12:17:38 +0300

    Add 3 board

--- EyeApp/ContentView.swift
+++ EyeApp/ContentView.swift
@@ -9,8 +9,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var step = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Color("Baklajan")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack (spacing: 25) {
+                Text("Добро пожаловать")
+                    .font(.largeTitle)
+                    .foregroundColor(Color("Light"))
+                    .padding(.top)
+                Text("Твои глаза")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(Color("Light"))
+                
+                GeometryReader { geometry in
+                    HStack {
+                        VStack (spacing: 35) {
+                            Image("P1")
+                                .resizable()
+                            Text("Твои глаза в твоих руках")
+                                .padding()
+                                .fixedSize(horizontal: false, vertical: true)
+                                .animation(Animation.interpolatingSpring(stiffness: 40, damping: 7).delay(0.1))
+                        }
+                        .frame(width: geometry.frame(in: .global).width)
+                        
+                        VStack (spacing: 35) {
+                            Image("P2")
+                                .resizable()
+                            Text("Глазобилдер")
+                                .padding()
+                                .fixedSize(horizontal: false, vertical: true)
+                                .animation(Animation.interpolatingSpring(stiffness: 40, damping: 7).delay(0.1))
+                        }
+                        .frame(width: geometry.frame(in: .global).width)
+                        
+                        VStack (spacing: 35) {
+                            Image("P3")
+                                .resizable()
+                            Text("Будешь видеть с километра")
+                                .padding()
+                                .fixedSize(horizontal: false, vertical: true)
+                                .animation(Animation.interpolatingSpring(stiffness: 40, damping: 7).delay(0.1))
+                        }
+                        .frame(width: geometry.frame(in: .global).width)
+                    }
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color("Light"))
+                    .font(.title).padding(.vertical, 60)
+                }
+            }
+            
+        }
     }
 }
 
