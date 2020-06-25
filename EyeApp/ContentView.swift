1a402d3da8297c8a744f29a1bea9e45420719e25
Ivantyaj <39460276+Ivantyaj@users.noreply.github.com>
2020-06-25 13:18:52 +0300

    Finished base of the app

--- EyeApp/ContentView.swift
+++ EyeApp/ContentView.swift
@@ -62,6 +62,53 @@ struct ContentView: View {
                     .multilineTextAlignment(.center)
                     .foregroundColor(Color("Light"))
                     .font(.title).padding(.vertical, 60)
+                    .frame(width: geometry.frame(in: .global).width * 3)
+                    .frame(maxHeight: .infinity)
+                    .offset(x: self.step == 1 ? geometry.frame(in: .global).width : self.step == 2 ? 0 : -geometry.frame(in: .global).width)
+                    .animation(Animation.interpolatingSpring(stiffness: 40, damping: 8))
+                }
+                HStack(spacing: 20){
+                    Button(action: {
+                        self.step = 1
+                    }) {
+                        Image(systemName: "1.circle")
+                            .padding()
+                            .scaleEffect(step == 1 ? 1 : 0.65)
+                    }
+                    Button(action: {
+                        self.step = 2
+                    }) {
+                        Image(systemName: "2.circle")
+                            .padding()
+                            .scaleEffect(step == 2 ? 1 : 0.65)
+                    }
+                    Button(action: {
+                        self.step = 3
+                    }) {
+                        Image(systemName: "3.circle")
+                            .padding()
+                            .scaleEffect(step == 3 ? 1 : 0.65)
+                    }
+                }
+                .animation(.spring(response: 0.4, dampingFraction: 0.5))
+                .font(.largeTitle)
+                .accentColor(Color("Light"))
+                
+                Button(action: {
+                    //                    go to next screen
+                }) {
+                    HStack {
+                        Text("Продолжить")
+                        Image(systemName: "chevron.right")
+                    }
+                    .padding(.horizontal)
+                    .padding()
+                    .background(Capsule().fill(Color("ButtonColor")))
+                    .accentColor((Color("Light")))
+                    .opacity(step == 3 ? 1 : 0)
+                    .animation(.none)
+                    .scaleEffect(step == 3 ? 1 : 0.01)
+                    .animation(Animation.interpolatingSpring(stiffness: 50, damping: 10, initialVelocity: 10))
                 }
             }
             
