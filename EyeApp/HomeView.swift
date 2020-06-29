b6c07e16bdba844f4f0495df68e7adf037105d56
Ivantyaj <39460276+Ivantyaj@users.noreply.github.com>
2020-06-29 22:33:47 +0300

    add button on homeview

--- EyeApp/HomeView.swift
+++ EyeApp/HomeView.swift
@@ -10,10 +10,61 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+        ZStack{
+            Color(#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1))
                 .edgesIgnoringSafeArea(.all)
-            Text("Следующий экран")
+            
+            GeometryReader{ geometry in
+                VStack {
+                    Button(action: {
+                        print("1")
+                    }) {
+                        ZStack {
+                            Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+                            VStack {
+                                Image(systemName: "eyeglasses")
+                                    .resizable()
+                                    .frame(width: geometry.size.width/2, height: geometry.size.width/2)
+                                    .accentColor(Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)))
+                                Text("Упражнения")
+                                    .font(.largeTitle)
+                                    .foregroundColor(Color.black)
+                            }
+                            
+                        }
+                        .cornerRadius(30)
+                        .padding()
+                        
+                    }
+                    .frame(width: geometry.size.width, height: geometry.frame(in: .global).height/2)
+                    
+                    
+                    
+                    Button(action: {
+                        print("2")
+                    }) {
+                        ZStack {
+                            Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+                            VStack {
+                                Image(systemName: "star.circle")
+                                    .resizable()
+                                    .frame(width: geometry.size.width/2, height: geometry.size.width/2)
+                                    .accentColor(Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)))
+                                Text("Тесты")
+                                    .font(.largeTitle)
+                                    .foregroundColor(Color.black)
+                            }
+                            
+                        }
+                        .cornerRadius(30)
+                        .padding()
+                    }
+                    .frame(width: geometry.size.width, height: geometry.frame(in: .global).height/2)
+                }
+                .padding(.horizontal)
+                
+                
+            }
         }
     }
 }
